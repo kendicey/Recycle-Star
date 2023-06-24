85e08dcef1e2098e2fb3e3176e566f5fb3412c45
kendice <kendiceyeungnm@gmail.com>
2023-06-24 15:04:58 -0700

    commit

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,6 @@
-const recyclable_item = document.querySelectorAll(".recyclable_item");
+const recyclable_item = document.querySelectorAll(".items");
 const recycle_bins = document.querySelectorAll(".bins");
+const recyclable_item_container = document.querySelector(".recyclable_item_container");
 
 const dragStart = (evt) => {
     evt.dataTransfer.setData('text/plain', evt.target.id);
@@ -21,7 +22,16 @@ const drop = (evt) => {
   
     if (isCorrectBin(droppedItemId, binId)) {
         const droppedItem = document.getElementById(droppedItemId);
+        console.log()
         droppedItem.classList.add('hidden');
+        score++;
+        if (numberRecord.length < 10){
+            const nextItem = pickItem();
+            recyclable_item_container.removeChild();
+            recyclable_item_container.insertAdjacentHTML("afterbegin", nextItem);
+        } else {
+            alert(`Congratulations! You are now a Recycle Star!`);
+        }
     } else {
         const droppedItem = document.getElementById(droppedItemId);
         droppedItem.classList.remove('hidden');
@@ -46,11 +56,17 @@ const isCorrectBin = (itemId, binId) => {
 }
 
 const pickItem = () => {
-    fetch('../items.json')
+    return fetch('../items.json')
         .then((response) => response.json())
         .then((data) => {
             const items = data;
-            
+            const randNum = Math.floor(Math.random()*items.length);
+            while (numberRecord.includes(randNum)){
+                randNum = Math.floor(Math.random()*items.length);
+            }
+            numberRecord.push(randNum);
+            console.log(numberRecord);
+            return `<img id=${items[randNum].id} class="items" src=${items[randNum].src} alt=${items[randNum].alt} draggable="true">`;
         })
         .catch((err) => console.log('Error:', err))
 }
@@ -67,4 +83,5 @@ recycle_bins.forEach(bin => {
 
 let numberRecord = [];
 
+pickItem().then(data => recyclable_item_container.insertAdjacentHTML("afterbegin", data));
 
